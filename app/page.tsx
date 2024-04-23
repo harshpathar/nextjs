9ab62ca687f5e383ef3a81a1aa447060705011ec
harshpathar <61840455+harshpathar@users.noreply.github.com>
2024-04-23 13:45:28 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -147,7 +147,7 @@ export default function Home() {
         <p><strong>Assignment</strong></p>
 {/*         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          <p><strong>Assignment</strong></p>
+          <p style={{ fontSize: "24px" }}><strong>Assignment</strong></p>
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
