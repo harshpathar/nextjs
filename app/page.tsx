1818411088d8d179bbb3a6402ec287cee5c236b4
harshpathar <61840455+harshpathar@users.noreply.github.com>
2024-04-23 13:38:41 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -113,7 +113,7 @@
 // }
 
 
-// import Image from "next/image";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -147,8 +147,8 @@ export default function Home() {
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           <p><strong>Assignment</strong></p>
-{/*           src="/next.svg"
-          alt="Next.js Logo" */}
+          src="/next.svg"
+          alt="Next.js Logo"
           width={180}
           height={37}
           priority
